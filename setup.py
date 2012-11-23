d8ad7953e9f11545dd0f4ac48375eba2bad9bffe
d0c0nnor <dannyoc@gmail.com>
2012-11-23 21:50:56 +0000

    Tweaked setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
 
-DESC="""Jocelyn is shim that makes it easier to use the Processing_ core
-libraries from Jython.
+DESC="""
 
 Full documentation is available on `GitHub
 <https://github.com/d0c0nnor/jocelyn>`_
 
-.. _Processing: http://processing.org
 """
 
 setup(
@@ -16,7 +14,7 @@ setup(
     author="Danny O'Connor",
     author_email="dannyoc@gmail.com",
     long_description=DESC,
-
+    description="A shim that makes it easier to use the Processing core libraries from Jython.",
     packages=['jocelyn',
               'jocelyn.examples',
               'jocelyn.examples.library_example'],
